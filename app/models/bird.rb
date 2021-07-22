3d173e3dc997fe40b11388816f3d24b27143cfc1
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-07-22 09:53:18 -0400

    Completed

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,23 @@
 class Bird < ApplicationRecord
+    # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end 
+  
 end
